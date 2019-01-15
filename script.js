26a0d65d065f5ba04bcfb7a717f846066d20607c
Brandon Park <brandonpark206@gmail.com>
2019-01-15 12:06:07 -0800

    cleaning up code

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-/* information about jsdocs: 
-* param: http://usejsdoc.org/tags-param.html#examples
-* returns: http://usejsdoc.org/tags-returns.html
-* 
-/**
- * Listen for the document to load and initialize the application
- */
 $(document).ready(initializeApp);
 
-/**
- * Define all global variables here.  
- */
-/***********************
- * student_array - global array to hold student objects
- * @type {Array}
- * example of student_array after input: 
- * student_array = [
- *  { name: 'Jake', course: 'Math', grade: 85 },
- *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
- * ];
- */
 var inventory_array = [];
 
-/***************************************************************************************************
-* initializeApp 
-* @params {undefined} none
-* @returns: {undefined} none
-* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
-*/
 function initializeApp(){
       addClickHandlersToElements ();
       loadData ();
+}
+
+function addClickHandlersToElements(){
+      $("#addButton").on("click",handleAddClicked);
+      $("#cancelButton").on("click",handleCancelClick);
+      $("#getDataFromServerButton").on("click",handleGetDataClick);
+      $("tbody").on("click", "td .btn-warning", handleUpdateClick);
 }
 
 function loadData () {
@@ -37,73 +19,72 @@ function loadData () {
             url: "api/access.php",
             method: "POST",
             data: {
-                  // api_key: "IluXv1RI8a",
-                  // "force-failure":"request"
                   'action' : 'read'
             },
             dataType: "json",
             success: function (response) {
-                  console.log(response);
-                  // if(response.error.length > 0){
-                  //       var error_message = response.error.join();
-                  //       $('.modal_error_message').text(error_message);
-                  //       $('#errorModal').modal('show');
-                  // }
-                  // else{
-                        inventory_array = response.data;
-                        console.log("this is the student array: ",inventory_array);
-                        updateInventoryList(inventory_array);
-                        console.log(response);
-                        return(response);
-                  // }
+                  inventory_array = response.data;
+                  updateInventoryList(inventory_array);
+                  return(response);
             }
       };
 
       $.ajax(inventory_api_object);
 }
 
-/***************************************************************************************************
-* addClickHandlerstoElements
-* @params {undefined} 
-* @returns  {undefined}
-*     
-*/
-function addClickHandlersToElements(){
-      $("#addButton").on("click",handleAddClicked);
-      $("#cancelButton").on("click",handleCancelClick);
-      $("#getDataFromServerButton").on("click",handleGetDataClick);
-      // $("tbody").on("click", "td .btn-danger", handleDeleteClick);
-      $("tbody").on("click", "td .btn-warning", handleUpdateClick);
+function clearAddInventoryFormInputs(){
+      $("#id").val("");
+      $("#title").val("");
+      $("#description").val("");
+      $("#quantity").val("");
+      $("#price").val("");
 }
 
-/***************************************************************************************************
- * handleAddClicked - Event Handler when user clicks the add button
- * @param {object} event  The event object from the click
- * @return: 
-       none
- */
-function handleAddClicked(){
-      addInventory ();
-}
-/***************************************************************************************************
- * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
- * @param: {undefined} none
- * @returns: {undefined} none
- * @calls: clearAddStudentFormInputs
- */
+function renderProductOnDom(inventoryObject){
+      var table_row = $("<tr>");
+      var title_TD = $("<td>").text(inventoryObject.product_title).css({'word-break':'break-all', 'word-wrap':'break-word'});
+      var description_TD = $("<td>").text(inventoryObject.product_description).css({'word-break':'break-all', 'word-wrap':'break-word'});
+      var quantity_TD = $("<td>").text(inventoryObject.quantity).css({'word-break':'break-all', 'word-wrap':'break-word'});
+      var price_TD = $("<td>").text(`$${inventoryObject.price}`);
+      var edit_TD = $("<td>").css({'word-break':'break-all', 'word-wrap':'break-word'});
 
-function handleGetDataClick () {
-      var student_api_object = {
-            url: "http://s-apis.learningfuze.com/sgt/get",
-            method: "POST",
-            data: {api_key: "IluXv1RI8a"},
-            dataType: "json",
-            success: function (response) {
-                  console.log(response);
-            }
-      };
+      var deleteButton = $("<button>", {
+            class: "btn btn-danger",
+            "data-toggle" : "modal",
+            "data-target" : "#deleteModal"
+      }).css({"margin-bottom": "10px", "margin-right":"10px"});
+      
+      var deleteIcon = $("<span>", {
+            class: "glyphicon glyphicon-trash visible-xs"
+      });
+      
+      var deleteText = $("<span>", {
+            text: "Delete",
+            class: "visible-sm visible-md visible-lg"
+      });
+      
+      var updateButton = $("<button>", {
+            class: "btn btn-warning",
+      }).css({"margin-bottom": "10px", "margin-right":"10px"});
+      
+      var updateIcon = $("<span>", {
+            class: "glyphicon glyphicon-pencil visible-xs"
+      });
+      
+      var updateText = $("<span>", {
+            text: "Update",
+            class: "visible-sm visible-md visible-lg"
+      });
+        
+      deleteButton.append(deleteIcon, deleteText);
+      updateButton.append(updateIcon, updateText);
 
-      $.ajax(student_api_object);
+      $(deleteButton).click(() => handleDeleteClick(table_row, inventoryObject));
+
+      edit_TD.append(deleteButton, updateButton);
+
+      var currentProductRow = $(table_row).append(title_TD,description_TD, quantity_TD, price_TD, edit_TD);
+      $("tbody").append(table_row);     
 }
 
 function handleCancelClick(){
@@ -113,12 +94,11 @@ function handleCancelClick(){
       $(".product-addition .invalid-price").css("display", "none");
       clearAddInventoryFormInputs ();
 }
-/***************************************************************************************************
- * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- * @param {undefined} none
- * @return undefined
- * @calls clearAddStudentFormInputs, updateStudentList
- */
+
+function handleAddClicked(){
+      addInventory ();
+}
+
 function addInventory(){
       var inventoryObj = {};
       inventoryObj.id = $("#id").val();
@@ -126,7 +106,6 @@ function addInventory(){
       inventoryObj.product_description = $("#description").val();
       inventoryObj.quantity = $("#quantity").val();
       inventoryObj.price = $("#price").val();
-      // inventoryObj.price = `$` + inventoryObj.price;
 
       validateAddProduct (inventoryObj);
 }
@@ -194,79 +173,16 @@ function validateAddProduct (addedProduct) {
       }
           
       if(validationCheck.product_title && validationCheck.product_description && validationCheck.quantity && validationCheck.price) {
-            // debugger;
             inventory_array.push(addedProduct);
             clearAddInventoryFormInputs ();
             addProductToDB (addedProduct);
             updateInventoryList (inventory_array);
       }
 }
-/***************************************************************************************************
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
-function clearAddInventoryFormInputs(){
-      $("#id").val("");
-      $("#title").val("");
-      $("#description").val("");
-      $("#quantity").val("");
-      $("#price").val("");
-}
-/***************************************************************************************************
- * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param {object} studentObj a single student object with course, name, and grade inside
- */
-function renderProductOnDom(inventoryObject){
-      var table_row = $("<tr>");
-      var title_TD = $("<td>").text(inventoryObject.product_title).css({'word-break':'break-all', 'word-wrap':'break-word'});
-      var description_TD = $("<td>").text(inventoryObject.product_description).css({'word-break':'break-all', 'word-wrap':'break-word'});
-      var quantity_TD = $("<td>").text(inventoryObject.quantity).css({'word-break':'break-all', 'word-wrap':'break-word'});
-      var price_TD = $("<td>").text(`$${inventoryObject.price}`);
-      var edit_TD = $("<td>").css({'word-break':'break-all', 'word-wrap':'break-word'});
-
-      var deleteButton = $("<button>", {
-            class: "btn btn-danger",
-            "data-toggle" : "modal",
-            "data-target" : "#deleteModal"
-      }).css({"margin-bottom": "10px", "margin-right":"10px"});
-      
-      var deleteIcon = $("<span>", {
-            class: "glyphicon glyphicon-trash visible-xs"
-      });
-      
-      var deleteText = $("<span>", {
-            text: "Delete",
-            class: "visible-sm visible-md visible-lg"
-      });
-      
-      var updateButton = $("<button>", {
-            class: "btn btn-warning",
-      }).css({"margin-bottom": "10px", "margin-right":"10px"});
-      
-      var updateIcon = $("<span>", {
-            class: "glyphicon glyphicon-pencil visible-xs"
-      });
-      
-      var updateText = $("<span>", {
-            text: "Update",
-            class: "visible-sm visible-md visible-lg"
-      });
-        
-      deleteButton.append(deleteIcon, deleteText);
-      updateButton.append(updateIcon, updateText);
-
-      $(deleteButton).click(() => handleDeleteClick(table_row, inventoryObject));
-
-      edit_TD.append(deleteButton, updateButton);
-
-      var currentProductRow = $(table_row).append(title_TD,description_TD, quantity_TD, price_TD, edit_TD);
-      $("tbody").append(table_row);     
-}
 
 function handleDeleteClick (table_row, inventoryObject){
       var thisRowIndex = $(table_row).closest("tr").index();
       var currentProduct = inventory_array[thisRowIndex];
-      var index = inventory_array.indexOf(inventoryObject);
 
       $("#deleteModal .modal-title").text(`Edit Product: ${currentProduct.product_title}`);
       $("#deleteModal .product-title").val(currentProduct.product_title);
@@ -383,50 +299,11 @@ function validateUpdateProduct (updatedProduct, index) {
       }
 }
 
-/***************************************************************************************************
- * updateStudentList - centralized function to update the average and call student list update
- * @param students {array} the array of student objects
- * @returns {undefined} none
- * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
- */
 function updateInventoryList(inventory_array){
       $("tbody").empty();
       for(var i = 0; i < inventory_array.length; i++){
             renderProductOnDom(inventory_array[i]);
       }
-      calculateGradeAverage (inventory_array);
-}
-/***************************************************************************************************
- * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
- * @param: {array} students  the array of student objects
- * @returns {number}
- */
-function calculateGradeAverage(student_array){
-      var totalGradeScore = 0;
-      for(var index = 0; index < student_array.length; index++){
-            var combinedGradeScore = parseFloat(student_array[index].grade);
-            totalGradeScore += combinedGradeScore;
-      }
-      var average = totalGradeScore/student_array.length;
-      if(isNaN(average)){
-            average = 0;
-            var fixedAverage = average.toFixed(0);
-            renderGradeAverage (fixedAverage+"%");
-            return fixedAverage;
-      }
-      else{
-            var fixedAverage = average.toFixed(0);
-            renderGradeAverage (fixedAverage+"%");
-            return fixedAverage;
-      }
-}
-/***************************************************************************************************
- * renderGradeAverage - updates the on-page grade average
- * @param: {number} average    the grade average
- * @returns {undefined} none
- */
-function renderGradeAverage(average){
-      $(".avgGrade").text(average);
 }
 
 function addProductToDB (addedProduct){
@@ -434,24 +311,18 @@ function addProductToDB (addedProduct){
             url: "api/access.php",
             method: "POST",
             data: {
-                  // api_key: "IluXv1RI8a",
                   product_title: addedProduct.product_title,
                   product_description: addedProduct.product_description,
                   quantity: addedProduct.quantity,
                   price: addedProduct.price,
                   'action' : 'create'
-                  // "force-failure": "server"
             },
             dataType: "json",
             success: function (response) {
-                  console.log(response);
-                  console.log(response.responseText);
                   return(response);
             },
             error: function (response) {
-                  console.log(response);
-                  $('.modal_error_message').text(response.statusText);
-                  $('#errorModal').modal('show');
+                  console.log("error message: ",response);
             }
       };
       $.ajax(inventory_api_object);
@@ -462,31 +333,18 @@ function deleteFromDB (idIndex) {
             url: "api/access.php",
             method: "POST",
             data: {
-                  // api_key: "IluXv1RI8a",
                   id: idIndex,
                   'action' : 'delete'
-                  // "force-failure": "timeout"
             },
             dataType: "json",
             success: function (response) {
-                  console.log(response);
                   return(response);
             },
-            error: function (jqXHR,exception) {
-                  console.log(jqXHR,exception);
-                  var error_message = "";
-                  if(exception === "timeout"){
-                        error_message = "Time out error"
-                  }
-                  console.log(error_message);
-                  // setTimeout(function(){ 
-                  //       $('.modal_error_message').text("took too long to load response from database");
-                  //       $('#errorModal').modal('show');}, 2000);
+            error: function(response) {
+                  console.log("error message: ",response);
             }
       };
-      // setTimeout(function(){ 
-      //       $('.modal_error_message').text("took too long to load response from database");
-      //       $('#errorModal').modal('show');}, 2000);
+
       $.ajax(inventory_api_object);
       return;
 }
@@ -505,7 +363,6 @@ function updateProductToDB(product) {
               id: product.id
             },
             success: function(response) {
-              console.log(response);
               updateInventoryList(inventory_array);
               return(response);
             },
