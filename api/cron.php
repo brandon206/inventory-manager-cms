7799f0cfca29f79846be23e2c6c59d8d68bfa31f
Brandon Park <brandonpark206@gmail.com>
2019-02-06 16:48:26 -0800

    Changing data inside cron.php to have new dummy data

--- api/cron.php
+++ api/cron.php
@@ -7,13 +7,18 @@ $truncateSQL = "TRUNCATE `Inventory`";
 mysqli_query($conn, $truncateSQL);
 
 $insertSQL = "INSERT INTO `Inventory` (`id`, `product_title`, `product_description`, `quantity`, `price`) VALUES
-(11, 'scriptljsdflkjdalfkjasd', 'a lawnmower that takes care of even the toughest weeds', 1000, '35.89'),
-(12, 'apple pies', 'delicious pie from apples', 750, '10.99'),
-(17, 'xhghg', 'xhghgxhghgxhghg', 1500, '5.99'),
-(19, 'asdkjflkds', 'laksdhfnlksdanmflkdsnflksdjankflmsdakflnsdlkfjnsdlflsk', 1000, '5.99'),
-(20, 'Apple Watch', 'Newest apple watch that includes Siri', 750, '6.00'),
-(21, 'this is the title', 'this is the description', 1500, '5.00'),
-(22, 'asdf', 'asdf', 12, '12.00')";
+(22, 'Stardust', 'Stardust - Gold frames feature a wayfarer shape with metal.', 300, '30.00'),
+(23, 'Andromeda', 'Andromeda - Patterned frames feature a rounded style.', 450, '27.99'),
+(24, 'Finale', 'Finale - Gold frames feature a two-toned, geometric style. A unique and vibrant look.', 550, '29.99'),
+(25, 'Moonlight', 'Moonlight - Navy frames feature a rounded-rectangular style.', 750, '19.99'),
+(26, 'Cascade Gold', 'Cascade is the perfect round frame that is simple enough for any occasion.', 1000, '22.50'),
+(27, 'Conifer Black', 'Conifer - Brass glasses feature a simple round wire frame.', 150, '28.99'),
+(28, 'Star Gold', 'Star - Gold frames boast a new circular shape with gold metal frames', 460, '5.42'),
+(29, 'Hourglass', 'Hourglass Metallic Black - Black rounded frames for a unique look', 1500, '32.00'),
+(30, 'Clubmaster', 'Clubmaster - Half circle frames that feature a sophisticated look', 255, '25.95'),
+(31, 'Clubmaster', 'Clubmaster - sophisticated frames for the bold', 550, '32.95'),
+(32, 'Clubmaster', 'Clubmaster - frames that are for the sophisticated and bold', 750, '32.95');
+";
 
 mysqli_query($conn, $insertSQL);
 
